3148800ba7c1359470f8ebf7f792da34951e6614
Donald Morton <d3baseball2@gmail.com>
2017-02-18 20:07:09 -0500

    Cleared Room

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// load filesystem module
-var fs = require("fs");
-
-// read JS file and execute it
-var data = fs.readFileSync("access.js", {encoding: "utf8"});
-eval(data);
-
-// write data out to files
-fs.writeFile("result1.json", JSON.stringify(an_array1));
