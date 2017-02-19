15844a4e9480685c367ab278c09c1a07c06b957f
Peter Cunha <petercunha8@gmail.com>
2017-02-19 17:14:31 -0500

    Add text-to-speech, tweak UI

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var request = require('request');
+var googleTTS = require('google-tts-api');
 
 var app = express();
 
@@ -25,7 +26,9 @@ app.set('view engine', 'hjs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -34,20 +37,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 io.on('connection', function(socket) {
@@ -60,7 +63,7 @@ io.on('connection', function(socket) {
     });
 });
 
-function wolframQuery(query, socket) {
+function wolframQuery(query, socket, url) {
     var result;
     var options = {
         url: 'https://api.wolframalpha.com/v1/result?appid=XVVT6H-237WH8JWQY&i=' + encodeURI(query),
@@ -69,14 +72,32 @@ function wolframQuery(query, socket) {
 
     function callback(error, response, body) {
         if (!error && response.statusCode == 200) {
-            socket.emit('message', {
-                stuff: body
+            getAudioURL(body).then(function(url) {
+              socket.emit('message', {
+                  stuff: body,
+                  audioURL: url
+              });
+              console.log(body);
             });
-            console.log(body);
+
         }
     }
     request(options, callback);
 }
 
+function getAudioURL(text) {
+  return new Promise(function(resolve, reject) {
+
+    googleTTS(text, 'en', 1) // Speed = 1
+        .then(function(url) {
+            console.log(url);
+            resolve(url);
+        })
+        .catch(function(err) {
+            console.error(err.stack);
+            reject("Could not retrieve audio URL.");
+        });
+  });
+}
 
 module.exports = app;
--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,14 +1,19 @@
 /*
  * VARIABLES, BABY!
  */
-var socket = io.connect();                     // Our dearest socket
-var TIMEOUT = 4                                // How long to wait for a server response before giving up
-var THINKING = false;                          // Is ReBeatal processing a request right now?
-var TEXTBOX = document.getElementById('ask');  // The question textbox
+var socket = io.connect(); // Our dearest socket
+var TIMEOUT = 4 // How long to wait for a server response before giving up
+var THINKING = false; // Is ReBeatal processing a request right now?
+var TEXTBOX = document.getElementById('ask'); // The question textbox
 
+
+// Play MP3 from URL
+function playSound(url) {
+    document.getElementById('audio').innerHTML = '<audio rel="noreferrer" src="' + url + '" autoplay></audio>';
+}
 
 // Enter key press fuction
-$(document).keypress(function (event) {
+$(document).keypress(function(event) {
     if ((event.which == 13 || event.keyCode == 13) && !THINKING) {
 
         // Do this suff when they hit enter
@@ -26,42 +31,47 @@ $(document).keypress(function (event) {
     return true;
 });
 
+// Function to query the server
 function queryServer(q) {
 
-  // Ask the server
-  socket.emit('query', q);
-  var didRespond = false;
+    // Ask the server
+    socket.emit('query', q);
+    var didRespond = false;
 
-  // On repsponse:
-  socket.on('message', function(msg) {
+    // On repsponse:
+    socket.on('message', function(msg) {
 
-      didRespond = true;
+        didRespond = true;
 
-      console.log("Got response from server: " + msg.stuff);
+        console.log("Got response from server: " + msg.stuff);
+        console.log(msg.audioURL);
 
-      TEXTBOX.disabled = false;
-      TEXTBOX.value = q;
-      TEXTBOX.focus();
+        TEXTBOX.disabled = false;
+        TEXTBOX.value = q;
+        TEXTBOX.focus();
 
-      if(THINKING)
-        alert(msg.stuff);
+        if (THINKING) {
+          playSound(msg.audioURL);
+          document.getElementById('overwrite').innerHTML = '<p class="lead">' + msg.stuff + '</p>';
+        }
 
-      // Keep this variable assignment at the bootom
-      THINKING = false;
-  });
 
-  // No response in var=TIMEOUT amount of seconds
-  setTimeout(function () {
-    if (!didRespond) {
+        // Keep this variable assignment at the bootom
+        THINKING = false;
+    });
 
-      TEXTBOX.disabled = false;
-      TEXTBOX.value = "";
-      TEXTBOX.focus();
+    // No response in var=TIMEOUT amount of seconds
+    setTimeout(function() {
+        if (!didRespond) {
 
-      alert("Hey buddy, how about you ask me a question that actually makes sense.");
+            TEXTBOX.disabled = false;
+            TEXTBOX.value = "";
+            TEXTBOX.focus();
 
-      // Keep this variable assignment at the bootom
-      THINKING = false;
-    }
-  }, TIMEOUT*1000);
+            alert("Hey buddy, how about you ask me a question that actually makes sense.");
+
+            // Keep this variable assignment at the bootom
+            THINKING = false;
+        }
+    }, TIMEOUT * 1000);
 }
